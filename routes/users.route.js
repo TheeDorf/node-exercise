3e66872a130d112c01b735d820ea4bb13d009722
AJ Dorfman <ajdorfman91@gmail.com>
2023-04-11 12:04:06 -0700

    fix code

--- routes/users.route.js
+++ routes/users.route.js
@@ -3,54 +3,57 @@ import db from "../mockdb/index.js";
 
 const userRouter = express.Router();
 
-userRouter.get('/:id?', async (req, res, next) => {
-    try {  
-      let { id } = req.params;
+userRouter.get("/:id?", async (req, res, next) => {
+  try {
+    let { id } = req.params;
     let data;
     if (id) {
-       data = await db.getOne(id);
-
+      data = await db.getOne(id);
     } else {
-       data = await db.getAll();
+      data = await db.getAll();
     }
     res.json(data);
-       
-    } catch (error) {
-       next(error);
-    }
- });
- 
- userRouter.post('/', async (req, res, next) => {
-    try {
-      let newUser = req.body;
-      let data = await db.add(newUser);
-      res.json(data);
-    } catch (error) {
-       next(error);
-    }
- });
- 
- userRouter.put('/:id', async (req, res, next) => {
-    try {
-     let { id } = req.params;
-     let updatedUser = req.body;
-     let data = await db.updated(id, updatedUser);
-      
-     res.json(data);
-    } catch (error) {
-       next(error);
-    }
- });
- 
- userRouter.delete('/:id', async (req, res, next) => {
-    try {
-       let { id } = req.params;
-       let data = await db.remove(id);
+  } catch (error) {
+    next(error);
+  }
+});
 
-       res.json(data); 
-    } catch (error) {
-       next(error);
-    }
- });
+userRouter.post("/", async (req, res, next) => {
+  
+   try {
+      console.log(req.body.firstName)
+    let newUser = req.body;
+    console.log(newUser);
+    let data = await db.add(newUser);
+    res.json(data);
+   } catch (error) {
+      next(error);
+   }
+});
+
+userRouter.put("/:id", async (req, res, next) => {
+   
+   try {
+    let { id } = req.params;
+    let updatedUser = req.body;
+    data = await db.update(id, updatedUser);
+
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.delete("/:id", async (req, res, next) => {
+ 
+   try {
+    let { id } = req.params;
+   let  data = await db.remove(id);
+
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default userRouter;
