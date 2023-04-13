ac5d4c55c1afe804e871cad59311e3d07699e783
AJ Dorfman <ajdorfman91@gmail.com>
2023-04-13 09:13:13 -0700

    add code and complete exercise

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRouter from "./users.route.js"
+import employeeRouter from "./employees.routes.js"
 // TODO: import router from users.route
 
 const router = express.Router();
@@ -8,7 +9,7 @@ router.get("/test", (req, res) => {
   res.send("working");
 });
 
-router.use("/users",userRouter)
+router.use("/users",userRouter);
 // TODO: use the imported router to handle all routes matching "/users"
-
+router.use("/employees", employeeRouter);
 export default router;
